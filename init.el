fdf1ad6fc483be7a12daeca09b90389784349b7f
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2021-04-23 22:19:33 +0300

    Initial commit

---
+++ init.el
@@ -0,0 +1,36 @@
+;; -*- lexical-binding: t; -*-
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(completion-styles '(orderless))
+ '(custom-enabled-themes '(almost-mono-black))
+ '(custom-safe-themes
+   '("8f5b54bf6a36fe1c138219960dd324aad8ab1f62f543bed73ef5ad60956e36ae" "021321ae56a45794f43b41de09fb2bfca184e196666b7d7ff59ea97ec2114559" default))
+ '(fringe-mode '(nil . 0) nil (fringe))
+ '(marginalia-mode t)
+ '(package-archives
+   '(("gnu" . "https://elpa.gnu.org/packages/")
+     ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+     ("melpa" . "https://melpa.org/packages/")))
+ '(package-selected-packages
+   '(exec-path-from-shell cider clojure-mode corfu almost-mono-themes consult marginalia orderless vertico eink-theme which-key))
+ '(save-place-mode t)
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(tab-always-indent 'complete)
+ '(tool-bar-mode nil)
+ '(vertico-mode t)
+ '(corfu-mode t)
+ '(which-key-mode t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :extend nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Rec Mono Duotone")))))
